42bc102976b70d517469725bf7088e61e2050b33
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-02-02 18:21:14 +0530

    new

---
+++ library/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <>
+    <Button className="bg-[#e7c9a5] text-black">Click me</Button>
+    </>
+  );
+};
+
+export default Home;
--- library/eslint.config.mjs
+++ library/eslint.config.mjs
@@ -1,18 +1,26 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import tailwind from "eslint-plugin-tailwindcss";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+
+  {
+    plugins: {
+      tailwindcss: tailwind,
+    },
+    rules: {
+      ...tailwind.configs.recommended.rules,
+      "no-undef": "off",
+    },
+  },
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
 ]);
-
-export default eslintConfig;
