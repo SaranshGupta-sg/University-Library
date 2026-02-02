57eb1a2ccee67f7143ccd9092b271c176a1aebb5
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-02-03 01:47:00 +0530

    new

--- library/app/(root)/layout.tsx
+++ library/app/(root)/layout.tsx
@@ -7,7 +7,7 @@ const layout = ({ children }: { children: ReactNode }) => {
   <>
   <main className="flex min-h-screen flex-1 flex-col bg-pattern bg-cover bg-top bg-dark-100 px-5 xs:px-10 md:px-16">
     <div className="mx-auto max-w-7xl">
-        <Header />
+        <Header/>
     <div className="mt-20 pb-20">{children}</div>
     </div>
   </main>
