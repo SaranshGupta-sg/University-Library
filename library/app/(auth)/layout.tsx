5fe2c8f969ad4e6a52b8a1474ea07fbab17342ec
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-02-16 20:17:33 +0530

    new

---
+++ library/app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ReactNode } from "react";
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Layout;
--- library/components/BookCard.tsx
+++ library/components/BookCard.tsx
@@ -13,40 +13,41 @@ const BookCard = ({
   cover,
   isLoanedBook = false,
 }: Book) => (
-  <li className="flex justify-center">
-    {/* card wrapper */}
-    <div className="w-40 xs:w-52 flex flex-col items-center">
-      <Link href={`/books/${id}`} className="flex flex-col items-center w-full">
-        <BookCover coverColor={color} coverImage={cover} />
+  <li className={cn(isLoanedBook && "sm:w-52 w-full")}>
+    <Link
+      href={`/books/${id}`}
+      className={cn(isLoanedBook && "w-full flex flex-col items-center")}
+    >
+      <BookCover coverColor={color} coverImage={cover} />
 
-        <div className="mt-4 w-full text-center xs:max-w-40 max-w-28">
-          <p className="mt-2 line-clamp-1 text-base font-semibold text-white xs:text-xl">
-            {title}
-          </p>
-          <p className="mt-1 line-clamp-1 text-sm italic text-light-100 xs:text-base">
-            {genre}
-          </p>
-        </div>
+      <div className={cn("mt-4", !isLoanedBook && "sm:max-w-40 max-w-28")}>
+        <p className="mt-2 line-clamp-1 text-base font-semibold text-white sm:text-xl">
+          {title}
+        </p>
+        <p className="mt-1 line-clamp-1 text-sm italic text-light-100 sm:text-base">
+          {genre}
+        </p>
+      </div>
 
-        {isLoanedBook && (
-          <div className="mt-3 w-full">
-            <div className="flex items-center gap-2 max-xs:justify-center">
-              <Image
-                src="/icons/calendar.svg"
-                alt="calendar"
-                width={18}
-                height={18}
-              />
-              <p className="text-white">11 days left to return</p>
-            </div>
-
-            <Button className="bg-black mt-3 min-h-14 w-full font-sans text-base text-[#E7C9A5] cursor-pointer">
-              Download receipt
-            </Button>
+      {isLoanedBook && (
+        <div className="mt-3 w-full">
+          <div className="flex items-center gap-2 max-sm:justify-center">
+            <Image
+              src="/icons/calendar.svg"
+              alt="calendar"
+              width={18}
+              height={18}
+              className="object-contain"
+            />
+            <p className="text-white">11 days left to return</p>
           </div>
-        )}
-      </Link>
-    </div>
+
+          <Button className="bg-black mt-3 min-h-14 w-full font-sans text-base text-[#E7C9A5] cursor-pointer">
+            Download receipt
+          </Button>
+        </div>
+      )}
+    </Link>
   </li>
 );
 
