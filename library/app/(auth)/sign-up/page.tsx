4bd1c806f697a55252ca79b5de90d0c8c488db40
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-02-17 23:01:34 +0530

    new

--- library/app/(auth)/sign-up/page.tsx
+++ library/app/(auth)/sign-up/page.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+"use client";
 
-const Page = () => {
-  return (
-    <div>
-    </div>
-  )
-}
+import AuthForm from "@/components/AuthForm";
+import { signUpSchema } from "@/lib/validations";
+// import { signUp } from "@/lib/actions/auth";
 
-export default Page
+const Page = () => (
+  <AuthForm
+    type="SIGN_UP"
+    schema={signUpSchema}
+    defaultValues={{
+      email: "",
+      password: "",
+      fullName: "",
+      universityId: 0,
+      universityCard: "",
+    }}
+    onSubmit={() => {}}
+  />
+);
+
+export default Page;
--- library/components/AuthForm.tsx
+++ library/components/AuthForm.tsx
@@ -1,18 +1,105 @@
-import React from "react";
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  DefaultValues,
+  FieldValues,
+  Path,
+  SubmitHandler,
+  useForm,
+  UseFormReturn,
+} from "react-hook-form";
+import { z, ZodType } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Props } from "recharts/types/container/Surface";
 
-// interface Props<T extends FieldValues> {
-//   schema: ZodType<T>;
-//   defaultValues: T;
-//   onSubmit: (data: T) => Promise<{ success: boolean; error?: string }>;
-//   type: "SIGN_IN" | "SIGN_UP";
-// }
+// import { Props } from "recharts/types/container/Surface";
 
-const AuthForm = ({ type, schema, defaultValues, onSubmit }: Props) => {
-  return <div></div>;
+import { useRouter } from "next/navigation";
+
+interface Props<T extends FieldValues> {
+  schema: ZodType<T, any>;
+  defaultValues: T;
+  onSubmit: (data: T) => Promise<{ success: boolean; error?: string }>;
+  type: "SIGN_IN" | "SIGN_UP";
+}
+
+const AuthForm = <T extends FieldValues>({
+  type,
+  schema,
+  defaultValues,
+  onSubmit,
+}: Props<T>) => {
+  const isSignIn = type === "SIGN_IN";
+
+  const form: UseFormReturn<T> = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: defaultValues as DefaultValues<T>,
+  });
+
+  const handleSubmit: SubmitHandler<T> = async (data) => {};
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-semibold text-white">
+        {isSignIn ? "Welcome back to BookWise" : "Create your library account"}
+      </h1>
+      <p className="text-[#D6E0FF]">
+        {isSignIn
+          ? "Access the vast collection of resources, and stay updated"
+          : "Please complete all fields and upload a valid university ID to gain access to the library"}
+      </p>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="w-full space-y-6"
+        >
+          {Object.keys(defaultValues).map((field) => (
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          ))}
+
+          <Button
+            type="submit"
+            className="bg-[#E7C9A5] text-[#16191E] hover:bg-[#E7C9A5] inline-flex min-h-14 w-full items-center justify-center rounded-md px-6 py-2 font-bold text-base"
+          >
+            Submit
+          </Button>
+        </form>
+      </Form>
+      <p className="text-center text-base font-medium">
+        {isSignIn ? "New to BookWise? " : "Already have an account? "}
+
+        <Link
+          href={isSignIn ? "/sign-up" : "/sign-in"}
+          className="font-bold text-[#E7C9A5]"
+        >
+          {isSignIn ? "Create an account" : "Sign in"}
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default AuthForm;
